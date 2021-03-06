ff9c4c36624e33e9dfb8cde922de27bba24826c4
Mike Nisbet <nisbet.mr@gmail.com>
2021-03-06 00:29:14 +0000

    splatter 2sided brush basics

--- src/TiltLoader.js
+++ src/TiltLoader.js
@@ -366,6 +366,39 @@ const shaders = {
 		blendSrc: 201,
 		blendSrcAlpha: 201,
 	},
+	'Splatter': {
+		uniforms: {
+			u_SceneLight_0_matrix: { value: [0.2931, 0.5524, -0.7803, 0, -0.8769, 0.4805, 0.0107, 0, 0.3809, 0.6811, 0.6253, 0, -4.9937, 8.1874, -46.2828, 1] },
+			u_SceneLight_1_matrix: { value: [0.1816, -0.1369, -0.9738, 0, -0.7915, -0.6080, -0.0621, 0, -0.5835, 0.7821, -0.2188, 0, -5.6205, 8.2530, -46.8315, 1] },
+			u_ambient_light_color: { value: new Vector4(0.3922, 0.3922, 0.3922, 1) },
+			u_SceneLight_0_color: { value: new Vector4(0.7780, 0.8157, 0.9914, 1) },
+			u_SceneLight_1_color: { value: new Vector4(0.4282, 0.4212, 0.3459, 1) },
+			u_MainTex: { value: loader.load( 'brushes/Splatter-8dc4a70c-d558-4efd-a5ed-d4e860f40dc3/Splatter-8dc4a70c-d558-4efd-a5ed-d4e860f40dc3-v10.0-MainTex.png', 
+				function (texture) {
+					texture.wrapS = RepeatWrapping;
+					texture.wrapT = RepeatWrapping;
+					texture.flipY = false;
+				}) 
+			},
+			u_Cutoff: { value: 0.2000 },
+			u_fogColor: { value: new Vector3(0.0196, 0.0196, 0.0196) },
+			u_fogDensity: { value: 0 }
+		},
+		vertexShader: TiltBrushShaders.Splatter.Vert,
+		fragmentShader: TiltBrushShaders.Splatter.Frag,
+		side: 2,
+		transparent: true,
+		depthFunc: 2,
+		depthWrite: true,
+		depthTest: true,
+		blending: 0,
+		blendDst: 201,
+		blendDstAlpha: 201,
+		blendEquation: 100,
+		blendEquationAlpha: 100,
+		blendSrc: 201,
+		blendSrcAlpha: 201,
+	},
 
 };
 
@@ -377,6 +410,8 @@ function getMaterial( GUID ) {
 
 		case 'Light':
 			return new RawShaderMaterial( shaders.Light );
+		case 'Splatter':
+			return new RawShaderMaterial( shaders.Splatter );
 
 		default:
 			return new MeshBasicMaterial( { vertexColors: true, side: DoubleSide } );
--- src/tiltbrush/tiltbrushshaders.js
+++ src/tiltbrush/tiltbrushshaders.js
@@ -1,11 +1,18 @@
 import lightVert from './brushes/Light-2241cd32-8ba2-48a5-9ee7-2caef7e9ed62/Light-2241cd32-8ba2-48a5-9ee7-2caef7e9ed62-v10.0-vertex.glsl'
 import lightFrag from './brushes/Light-2241cd32-8ba2-48a5-9ee7-2caef7e9ed62/Light-2241cd32-8ba2-48a5-9ee7-2caef7e9ed62-v10.0-fragment.glsl'
 
+import splatterVert from './brushes/Splatter-8dc4a70c-d558-4efd-a5ed-d4e860f40dc3/Splatter-8dc4a70c-d558-4efd-a5ed-d4e860f40dc3-v10.0-vertex.glsl'
+import splatterFrag from './brushes/Splatter-8dc4a70c-d558-4efd-a5ed-d4e860f40dc3/Splatter-8dc4a70c-d558-4efd-a5ed-d4e860f40dc3-v10.0-fragment.glsl'
+
 const TiltBrushShaders = {
     'Light': {
         'Vert': lightVert,
         'Frag': lightFrag
     },
+    'Splatter': {
+        'Vert': splatterVert,
+        'Frag': splatterFrag
+    }
 }
 
 export { TiltBrushShaders };
