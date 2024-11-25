03f7d58a5ef4f9b97042f50d5265f833fad2f8ed
Andy Baker <andy@andybak.net>
2024-11-25 22:05:29 +0000

    "Frame scene" ui

--- src/viewer.ts
+++ src/viewer.ts
@@ -235,6 +235,8 @@ export class Viewer {
             this.icosa_frame.id = 'icosa-viewer';
         }
 
+        initCustomUi(this.icosa_frame);
+
         const controlPanel = document.createElement('div');
         controlPanel.classList.add('control-panel');
 
@@ -433,6 +435,40 @@ export class Viewer {
         // this.icosa_frame.appendChild( arButton );
         // arButton.style.left = `${parseInt(window.getComputedStyle(arButton).left, 10) + 150}px`;
 
+
+            const button = document.createElement('button');
+            button.innerHTML = `<?xml version="1.0" encoding="utf-8"?>
+<svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4 7.5L11.6078 3.22062C11.7509 3.14014 11.8224 3.09991 11.8982 3.08414C11.9654 3.07019 12.0346 3.07019 12.1018 3.08414C12.1776 3.09991 12.2491 3.14014 12.3922 3.22062L20 7.5M4 7.5V16.0321C4 16.2025 4 16.2876 4.02499 16.3637C4.04711 16.431 4.08326 16.4928 4.13106 16.545C4.1851 16.6041 4.25933 16.6459 4.40779 16.7294L12 21M4 7.5L12 11.5M12 21L19.5922 16.7294C19.7407 16.6459 19.8149 16.6041 19.8689 16.545C19.9167 16.4928 19.9529 16.431 19.975 16.3637C20 16.2876 20 16.2025 20 16.0321V7.5M12 21V11.5M20 7.5L12 11.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`;
+            // button.style.fontSize = '24px';
+            // button.style.color = 'white';
+            button.style.backgroundColor = 'black';
+            button.style.position = 'absolute';
+            button.style.bottom = '10px';
+            button.style.left = '10px';
+            button.style.padding = '0px 2px';
+            button.style.border = 'none';
+            button.style.color = 'white';
+            button.style.cursor = 'pointer';
+            button.style.zIndex = '1000'; // Ensure it's above the canvas
+            button.title = 'Fit Scene to View';
+            viewerContainer.appendChild(button);
+
+            button.addEventListener('click', () => {
+                console.log('Button clicked!');
+                viewer.frameScene()
+            });
+            // Hover effects
+            button.addEventListener('mouseover', () => {
+                button.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
+            });
+            button.addEventListener('mouseout', () => {
+                button.style.backgroundColor = 'black';
+            });
+
+
+        }
 
         function animate() {
             renderer.setAnimationLoop(render);
